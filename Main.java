ac3ffba04c742f37fe6d3534a228b3c512871596
Erick Omondi <eriqueasaria@gmail.com>
2023-11-26 23:52:19 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 package Roster;
 
 int days = 3
+  //ArrayList of ArrayLists
 ArrayList<ArrayList<String>> roster = new ArrayList<ArrayList<String>>(days);
 
 ArrayList <String> day1 = new ArrayList<>();
