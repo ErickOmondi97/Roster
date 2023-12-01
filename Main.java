7490502b41bd073abd6b1120ed7dd974421dbae0
Erick Omondi <eriqueasaria@gmail.com>
2023-12-01 23:39:26 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,3 +8,7 @@ ArrayList<ArrayList<String>> roster = new ArrayList<ArrayList<String>>(days);
 ArrayList <String> day1 = new ArrayList<>();
 ArrayList <String> day2 = new ArrayList<>();
 ArrayList <String> day3 = new ArrayList<>();
+
+//add the days to the arraylists
+day1.add("Milani");
+day2.add("Amor");
