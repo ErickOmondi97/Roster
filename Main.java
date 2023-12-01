03b4a61f99bf74459b10f27a79a8bb8b2784c7fa
Erick Omondi <eriqueasaria@gmail.com>
2023-12-01 23:41:03 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,18 @@
 package Roster;
 
-int days = 3
-  //ArrayList of ArrayLists
-ArrayList<ArrayList<String>> roster = new ArrayList<ArrayList<String>>(days);
-
-  //each sub ArrayList
-ArrayList <String> day1 = new ArrayList<>();
-ArrayList <String> day2 = new ArrayList<>();
-ArrayList <String> day3 = new ArrayList<>();
-
-//add the days to the arraylists
-day1.add("Milani");
-day2.add("Amor");
+public static void main(String[] args){
+  int days = 3
+    //ArrayList of ArrayLists
+  ArrayList<ArrayList<String>> roster = new ArrayList<ArrayList<String>>(days);
+  
+    //each sub ArrayList
+  ArrayList <String> day1 = new ArrayList<>();
+  ArrayList <String> day2 = new ArrayList<>();
+  ArrayList <String> day3 = new ArrayList<>();
+  
+  //add the days to the arraylists
+  day1.add("Milani");
+  day2.add("Amor");
+  day3.add("Erick");
+  day4.add("Omondi");
+}
